e26b53620434b18b1a1990b7e6d4a1b8bab84e06
Grant Kirill <webdotg.inbox@gmail.com>
2023-06-07 21:25:35 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BoxComponent from "./components/BoxWrapper";
+import LogIn from "./components/LogIn";
+
+function App() {
+		return (
+				<div className="App">
+						<LogIn />
+						<BoxComponent/>
+				</div>
+		);
+}
+
+export default App;
